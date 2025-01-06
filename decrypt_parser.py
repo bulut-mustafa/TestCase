a311cde5521aef0049d166379b1f80c5417c8fdf
Mustafa Bulut <mustafaiste@outlook.com>
2025-01-06 21:24:19 +0700

    first commit

---
+++ decrypt_parser.py
@@ -0,0 +1,41 @@
+import json
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+def extract_info(url: str, html_content: str) -> dict:
+    """"
+    A snippet to extract author, publish date, and update date from a Decrypt.co article.
+    """
+    
+    soup = BeautifulSoup(html_content, "html.parser")
+    site_name = soup.find(name='meta', attrs={'property': 'og:site_name'}).get('content')
+    info_script = soup.find('script', type='application/ld+json')
+    if not info_script:
+        return {'author': "Author not found", 'published': "Published date not found", 'latestUpdate': "Updated date not found"}
+    
+    json_info = json.loads(info_script.getText())
+    
+    metadata ={'url': url,
+               'publish_date' : json_info['datePublished'], 
+               'update_date' : json_info['dateModified'], 
+               'author' : json_info['author']['name'],
+               'site_name': site_name}
+    
+    
+    return metadata
+with open("dataset2.json", "r") as file:
+    data = json.load(file)
+
+medium_entries = [item for item in data if urlparse(item["url"]).netloc == "decrypt.co"]
+
+results = []
+
+for entry in medium_entries:
+    html_content = entry.get("html", "")
+    url_content = entry.get("url", "")
+    
+    extracted = extract_info(url_content,html_content)
+       
+    
+    results.append(extracted)
+
+print(json.dumps(results, indent=4))
---
+++ playtoearn_parser.py
@@ -0,0 +1,54 @@
+import json
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+def extract_info(url: str, html_content: str) -> dict:
+    """"
+    A snippet to extract author, publish date, and update date from a PlayToEarn.com article.
+    """
+    
+    soup = BeautifulSoup(html_content, "html.parser")
+    site_name  = soup.find(name='meta', attrs={'name': 'application-name'}).get('content')
+    author_tag = soup.find('div', class_='__CreatorName')
+    author = author_tag.getText() if author_tag else "Author not found"
+        
+    disclaimer_divs = soup.find_all("div", class_="NewsContentDisclaimer")
+    posted_date = "Not found"
+    updated_date = "Not found"
+
+    for div in disclaimer_divs:
+        if "Posted:" in div.text and "Updated:" in div.text:
+            text = div.text
+            posted_match = re.search(r"Posted: ([\d]{2} [A-Za-z]{3}, [\d]{4} [\d]{2}:[\d]{2})", text)
+            updated_match = re.search(r"Updated: ([\d]{2} [A-Za-z]{3}, [\d]{4} [\d]{2}:[\d]{2})", text)
+            posted_date = posted_match.group(1) if posted_match else "Not found"
+            updated_date = updated_match.group(1) if updated_match else "Not found"
+            break
+            
+            
+    result = {
+        "url": url_content,
+        "author": author,
+        "published": posted_date,
+        "latestUpdate": updated_date,
+        'siteName': site_name
+    }
+    return result
+with open("dataset2.json", "r") as file:
+    data = json.load(file)
+
+medium_entries = [item for item in data if urlparse(item["url"]).netloc == "playtoearn.com"]
+
+results = []
+
+for entry in medium_entries:
+    html_content = entry.get("html", "")
+    url_content = entry.get("url", "")
+    
+    extracted = extract_info(url_content,html_content)
+        
+    
+    results.append(extracted)
+
+print(json.dumps(results, indent=4))
